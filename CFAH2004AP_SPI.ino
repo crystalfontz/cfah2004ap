7671bf0c3d1c39baec7897a1f5f086039e42eecc
Max Roberg <max.roberg@gmail.com>
2017-09-29 16:08:00 -0700

    Added CGRAM example

--- CFAH2004AP_SPI.ino
+++ CFAH2004AP_SPI.ino
@@ -37,14 +37,13 @@
 //----------------------------------------------------------------------------
 #define Cword 0x14  //20
 #define LINE_ONE    0x80  // DD RAM Address The starting position of the first line is 0x00
-// set the DD RAM address to 0x80 + 0x00 = 0x80
+                          // set the DD RAM address to 0x80 + 0x00 = 0x80
 #define LINE_TWO    0xc0  // DD RAM Address The starting position of the second line is 0x40
-// set the DD RAM address to 0x80 + 0x40 = 0xc0
+                          // set the DD RAM address to 0x80 + 0x40 = 0xc0
 #define LINE_THREE  0x94  // DD RAM Address The starting position of the second line is 0x14
-// set the DD RAM address to 0x80 + 0x14 = 0x94
+                          // set the DD RAM address to 0x80 + 0x14 = 0x94
 #define LINE_FOUR   0xD4  // DD RAM Address The starting position of the second line is 0x54
-// set the DD RAM address to 0x80 + 0x54 = 0xD4
-
+                          // set the DD RAM address to 0x80 + 0x54 = 0xD4
 //============================================================================
 // LCD SPI & control lines
 //   ARD      | Port | LCD
@@ -68,12 +67,24 @@
 
 //                //00000000001111111111
 // 								//01234567890123456789
-uint8_t MSG1[20]  ="**  CRYSTALFONTZ  **";
-uint8_t MSG2[20]  ="** CFAH2004AP-TMI **";
-uint8_t MSG3[20]  ="**  20 Characters **";
-uint8_t MSG4[20]  ="**   by 4 lines   **";
-uint8_t CGRAM1[8] ={0x04,0x0E,0x15,0x04,0x04,0x04,0x04,0x04,};
-uint8_t CGRAM2[8] ={0x04,0x04,0x04,0x04,0x04,0x15,0x0e,0x04,};
+uint8_t MSG1[20]    ="**  CRYSTALFONTZ  **";
+uint8_t MSG2[20]    ="** CFAH2004AP-TMI **";
+uint8_t MSG3_1[20]  ="**  20 Characters **";
+uint8_t MSG3_2[20]  ="** 8  CUSTOM CHAR **";
+uint8_t MSG4[20]    ="**   by 4 lines   **";
+uint8_t CGRAM1[8]   ={0x04,0x0E,0x15,0x04,0x04,0x04,0x04,0x04,}; //UP ARROW
+uint8_t CGRAM2[8]   ={0x04,0x04,0x04,0x04,0x04,0x15,0x0e,0x04,}; //DOWN ARROW
+
+uint8_t CGRAM[8][8] = {
+  {21, 42, 21, 42, 21, 42, 21, 42},
+  {42, 21, 42, 21, 42, 21, 42, 21},
+  {63, 31, 15,  7,  3,  1,  0,  0},
+  { 0,  0, 32, 48, 56, 60, 62, 63},
+  { 1,  2,  4,  8,  8,  4,  2,  1},
+  {32, 16,  8,  4,  4,  8, 16, 32},
+  {62, 61, 59, 55, 47, 31, 47, 55},
+  {59, 61, 62, 61, 59, 55, 47, 31},
+};
 //============================================================================
 void writeCommand(uint8_t command);
 void writeData(uint8_t data);
@@ -128,6 +139,26 @@ void Initialize_CFAH2004AP()
   delay(1);
 }
 //============================================================================
+void Initialize_CGRAM(void)
+{
+  Serial.println(F("CGRAM()"));
+  
+  unsigned char i, j;
+  // The first graph of the CGRAM Address start address
+  //  is 000000 (0x00) CGRAM address is set to 0x40 + 0x00 = 0x40
+  // The second graph's CGRAM Address starts with address 001000 (0x08)
+  // etc.
+  
+  for (i = 0; i < 8; i ++)
+  {
+    writeCommand(0x40 + (0x08 * i));
+    for (j = 0; j < 8; j ++)
+    {
+      writeData(CGRAM[i][j]);
+    }      
+  }
+}
+//============================================================================
 void setup( void )
 {
   // LCD SPI & control lines
@@ -164,14 +195,15 @@ void setup( void )
   //Fire up the SPI OLED
   Serial.println(F("Initialize_CFAH2004AP()"));
   Initialize_CFAH2004AP();
-
+  Initialize_CGRAM();
+  
   //From the init
   writeCommand(LINE_ONE);
 	writeString(Cword,MSG1);
 	writeCommand(LINE_TWO);
 	writeString(Cword,MSG2);
 	writeCommand(LINE_THREE);
-	writeString(Cword,MSG3);
+	writeString(Cword,MSG3_1);
 	writeCommand(LINE_FOUR);
 	writeString(Cword,MSG4);
 }
@@ -180,8 +212,10 @@ uint8_t currentLine[20] = "** CFAH2004AP-TMI **";
 uint8_t iteration = 0;
 void loop(void)
 {
+  Serial.println(F("Loop()"));
   uint8_t temp;
   
+  //This is the scrolling
   temp = currentLine[0];
   for(uint8_t thisChar = 1; thisChar < 20; thisChar++)
   {
@@ -191,11 +225,43 @@ void loop(void)
 	writeCommand(LINE_TWO);
   writeString(Cword,currentLine);
   iteration++;
-  delay(250);
+  delay(150);
 
+  //Scrolling is done
   if(iteration == 20)
   {
-    delay(2000);
+    Serial.println(F("20x Loop()"));
+    delay(1000);
+
+    //Show off the CGRAM
+	  writeCommand(LINE_THREE);
+	  writeString(Cword,MSG3_2);
+    
+    writeCommand(LINE_FOUR);
+    writeData('*');
+    writeData('*');
+    writeData(' ');
+    writeData(' ');
+    writeData(' ');
+    writeData(' ');
+    for(uint8_t thisChar = 0; thisChar < 8; thisChar++)
+    {
+      writeData(thisChar);
+    }
+    writeData(' ');
+    writeData(' ');
+    writeData(' ');
+    writeData(' ');
+    writeData('*');
+    writeData('*');
+    delay(10000);
+
+    //Put the text back
+    writeCommand(LINE_THREE);
+    writeString(Cword,MSG3_1);
+    writeCommand(LINE_FOUR);
+    writeString(Cword,MSG4);
+    delay(1000);
     iteration = 0;
   }
 }
